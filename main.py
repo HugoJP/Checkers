3d4acb66c36baee08a6ba04003e558a62c58f922
Hugo Jeanperrin <hugo.jeanperrin@zopa.com>
2016-06-12 13:20:12 +0100

    run from main

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
+import pygame.sprite
 
-(width, height) = (512, 512)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Checkers")
-background_colour = (255, 255, 255)
-screen.fill(background_colour)
-bg = pygame.image.load("checkers.jpg")
-screen.blit(bg, (0, 0))
+def Base(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
 
 
-pygame.display.flip()
+def start_game():
+    (width, height) = (512, 512)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Checkers")
+    background_colour = (255, 255, 255)
+    screen.fill(background_colour)
+    bg = pygame.image.load("checkers.jpg")
+    screen.blit(bg, (0, 0))
 
-running = True
-while running:
+    pygame.display.flip()
 
-    for event in pygame.event.get():
+    running = True
+    while running:
 
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+if __name__ == "__main__":
+    start_game()
+
